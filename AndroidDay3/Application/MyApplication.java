361705072e890154e0e5ceb97492afeba1bf00b4
MaINoo999 <135982856+MaINoo999@users.noreply.github.com>
2025-09-30 15:04:04 +0900

    Create MyApplication.java

---
+++ AndroidDay3/Application/MyApplication.java
@@ -0,0 +1,19 @@
+package com.iot.application;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    private String globalString;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        globalString = "Hello Android";
+    }
+    public String getGlobalString(){
+        return globalString;
+    }
+    public void setGlobalString(String globalString){
+        this.globalString = globalString;
+    }
+}
