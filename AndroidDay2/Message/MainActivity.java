d899969b9028b2303bc729f3bf99df772c002122
MaINoo999 <135982856+MaINoo999@users.noreply.github.com>
2025-09-30 14:12:01 +0900

    Create MainActivity.java

---
+++ AndroidDay2/Message/MainActivity.java
@@ -0,0 +1,58 @@
+package com.iot.message;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private ActivityResultLauncher<Intent> launcher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+
+        // Window Insets 패딩 처리
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            String resultData = data.getStringExtra("subMsg");
+                            if (resultData != null) {
+                                editText.setText(resultData);
+                            }
+                        }
+                    }
+                }
+        );
+
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SubActivity.class);
+            launcher.launch(intent);
+        });
+    }
+}
