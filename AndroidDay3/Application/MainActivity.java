4282d229d033fd2b6e30f2cf31c1e276ebb8f35c
MaINoo999 <135982856+MaINoo999@users.noreply.github.com>
2025-09-30 15:03:43 +0900

    Create MainActivity.java

---
+++ AndroidDay3/Application/MainActivity.java
@@ -0,0 +1,26 @@
+package com.iot.application;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        MyApplication myApplication = (MyApplication) getApplication();
+        String globalString = myApplication.getGlobalString();
+
+        textView.setText(globalString);
+    }
+}
